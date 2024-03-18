01bdb36fee4c0d5418737923223864e104efaeb6
AlvinObala <alvinomolo99@gmail.com>
2024-03-19 01:10:09 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance>=0 &&distance<=400){
+    return 'This one is on me!'
+  } else if (distance>400 && distance<=2000){
+      return 'That will be twenty bucks.'
+  }
+  else if (distance>2000 && distance<=2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else{
+    return 'No can do.'
+  }
+  
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  // condition ? <expression if true> : <expression if false>
+  let result;
+  result = city ==='NYC'? 'Ok, sounds good.' : 'No go.';
+  return result;
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result
+  switch (tip) {
+    case "generous":
+      result = 'Thank you so much.'
+      break;
+    case "not as generous":
+      result = 'Thank you.'
+      break;
+
+    default:
+      result = 'Bye.'
+  }
+  return result;
 }
+
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(1500);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
+
+ternaryCheckCity('NYC');
+ternaryCheckCity('Pittsburgh');
+
+switchOnCharmFromTip('generous');
+switchOnCharmFromTip('not so generous');
+switchOnCharmFromTip('no tip');
